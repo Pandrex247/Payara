d26e14a257b7df208e0f1a801a551380325d0432
Andrew Pielage <andrew.pielage@payara.fish>
2019-05-01 17:24:35 +0100

    PAYARA-3314 Got the instance starting finally

--- appserver/extras/docker-images/src/main/resources/Server-Full/entrypoint.sh
+++ appserver/extras/docker-images/src/main/resources/Server-Full/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo ${PAYARA_DAS_HOST}
-echo ${PAYARA_DAS_PORT}
-echo ${PAYARA_NODE_NAME}
-echo ${PAYARA_INSTANCE_NAME}
-
 # Check if instance already created before running command
 if [ ! -d "payara5/glassfish/nodes/${PAYARA_NODE_NAME}/${PAYARA_INSTANCE_NAME}" ]; then
-    ./payara5/bin/asadmin -H ${PAYARA_DAS_HOST} -p ${PAYARA_DAS_PORT} _create-instance-filesystem --node ${PAYARA_NODE_NAME} ${PAYARA_INSTANCE_NAME}
+    ./payara5/bin/asadmin -H ${PAYARA_DAS_HOST} -p ${PAYARA_DAS_PORT} _create-instance-filesystem --node ${PAYARA_NODE_NAME} --dockernode true ${PAYARA_INSTANCE_NAME}
 fi
 
-./payara5/bin/asadmin --passwordfile ${PAYARA_PASSWORD_FILE} start-local-instance ${PAYARA_INSTANCE_NAME}
+./payara5/bin/asadmin --passwordfile ${PAYARA_PASSWORD_FILE} start-local-instance --verbose ${PAYARA_INSTANCE_NAME}
