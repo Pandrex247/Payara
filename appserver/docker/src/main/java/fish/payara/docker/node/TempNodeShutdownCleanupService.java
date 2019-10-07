e24063303776c9de32acf9478eb52dca28b00d74
Andrew Pielage <andrew.pielage@payara.fish>
2019-10-07 12:18:30 +0100

    PAYARA-4099 missing runlevel

--- appserver/docker/src/main/java/fish/payara/docker/node/TempNodeShutdownCleanupService.java
+++ appserver/docker/src/main/java/fish/payara/docker/node/TempNodeShutdownCleanupService.java
@@ -1,13 +1,12 @@
 package fish.payara.docker.node;
 
-import com.sun.enterprise.config.serverbeans.Node;
-import com.sun.enterprise.config.serverbeans.Nodes;
-import fish.payara.nucleus.executorservice.PayaraExecutorService;
+import org.glassfish.api.StartupRunLevel;
 import org.glassfish.api.event.EventListener;
 import org.glassfish.api.event.EventTypes;
 import org.glassfish.api.event.Events;
 import org.glassfish.embeddable.CommandRunner;
 import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.runlevel.RunLevel;
 import org.glassfish.internal.api.Globals;
 import org.jvnet.hk2.annotations.Service;
 
@@ -15,6 +14,7 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 @Service(name = "temp-node-cleanup-service")
+@RunLevel(StartupRunLevel.VAL)
 public class TempNodeShutdownCleanupService implements EventListener {
 
     @Inject
